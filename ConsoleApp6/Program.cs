4d8d9d0ef0320fb738f64729b9591b478391f216
User <User@WIN-H01UE0AB088>
2026-02-05 09:47:39 +0300

    1.0

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -18,6 +18,11 @@ namespace ConsoleApp6
             //    sum += i;
             //}
 
+            //int[] mass = { 1, 3, 5, 7, 9 };
+            //int N = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < mass.Length; i++) {
+            //Console.WriteLine("Есть");
+            //}
 
             
         }
