4ac02abcf0b1dff90d42f23b0cd2672f98ef898c
User <User@WIN-H01UE0AB088>
2026-02-05 09:44:37 +0300

    123

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n= Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    sum += i;
+            //}
+
+            //int[] mass = { 1, 3, 5, 7, 9 };
+            //int N = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < mass.Length; i++) {
+            //Console.WriteLine("Есть");
+            //}
+
+            
+        }
+    }
+}
